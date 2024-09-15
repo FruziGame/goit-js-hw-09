71b7d2e2008399e483acd7cd632e838e020627b2
Hlib <grossogleb@gmail.coml>
2024-09-15 14:46:56 +0300

    'update'

---
+++ src/js/2-form.js
@@ -0,0 +1,62 @@
+
+const formData = {
+    email: "",
+    message: ""
+}
+
+const localStorageData = "feedback-form-state";
+const feedbackForm = document.querySelector(".feedback-form") 
+
+const email = feedbackForm.querySelector('input[type="email"]')
+const message = feedbackForm.querySelector('textarea[name="message"]')
+
+
+feedbackForm.addEventListener("input", (e) => {
+
+    if (e.target.name === "email") formData.email = e.target.value.trim();
+    if (e.target.name === "message") formData.message = e.target.value.trim();
+
+    localStorage.setItem(localStorageData, JSON.stringify(formData));
+ 
+ });
+
+
+
+ function firstStep() {
+    const storageData = localStorage.getItem(localStorageData);
+    const parsedData = storageData ? JSON.parse(storageData) : {};
+
+    if (parsedData.email !== undefined) {
+        feedbackForm.elements.email.value = parsedData.email;
+    }
+    if (parsedData.message !== undefined) {
+        feedbackForm.elements.message.value = parsedData.message;
+    }
+}
+
+firstStep()
+
+
+
+
+feedbackForm.addEventListener("submit", (e) => {
+
+    if (feedbackForm.elements.email.value.trim() === "" ||  feedbackForm.elements.message.value.trim() === "") {
+              
+           return alert("Fill please all fields.")
+        }
+
+
+    console.log(formData)
+    localStorage.removeItem(localStorageData);
+    formData.email = "";
+    formData.message = "";
+
+   
+
+})
+
+
+
+
+
